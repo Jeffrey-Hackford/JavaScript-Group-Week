42a61e787a1c85ba980743e61820f6a873b91c09
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-06-22 10:40:19 -0700

    add hide function for introConents upon search

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
     event.preventDefault();
     if(event.keyCode==13) {
       $('div').removeClass('hiddenBills');
+      $('div.introContent').addClass('hiddenIntroConent');
     }
   });
   $('.politicianSearch').keypress(function(event){
     event.preventDefault();
     if(event.keyCode==13) {
       $('div').removeClass('hiddenPoliticians');
+      $('div.introContent').addClass('hiddenIntroConent');
     }
   });
 });
