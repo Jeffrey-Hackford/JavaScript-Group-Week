1dbc358c68af40dfce72227285a95f39accfdfbb
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-06-23 09:47:35 -0700

    add styling to refresh button

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
   $('.billSearch').keypress(function(event){
     event.preventDefault();
     if(event.keyCode==13) {
-      $('div.hiddenBills').fadeIn(3000).removeClass('hiddenBills');
-      $('div.introContent').fadeOut(3000);
+      $('div.hiddenBills').fadeIn(7000).removeClass('hiddenBills');
+      $('div.introContent').fadeOut(4000);
     }
   });
   $('.politicianSearch').keypress(function(event){
     event.preventDefault();
     if(event.keyCode==13) {
-      $('div.hiddenPoliticians').fadeIn(3000).removeClass('hiddenPoliticians');
-      $('div.introContent').fadeOut(3000);
+      $('div.hiddenPoliticians').fadeIn(7000).removeClass('hiddenPoliticians');
+      $('div.introContent').fadeOut(4000);
     }
   });
 });
